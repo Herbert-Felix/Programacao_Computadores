cc80d456b80d90a5987d0c2cf35098bdb4095c3e
Herbert Felix <herbertfelix.dev@outlook.com>
2024-02-29 01:28:37 -0300

    Exercício aula 01

---
+++ Exercicio_01_Aula02.py
@@ -0,0 +1,17 @@
+""" 1-Faça um programa em Python que solicite ao usuário a sua profissão 
+    e mostre na tela a informação digitada"""
+
+""" 2-Faça um programa em Python que soliciteao usuário a sua idade 
+e apresente a informação na tela"""
+
+""" 3-Faça um programa em Python que solicite o último sobrenome do usuário e mostre na tela a
+mensagem e na sequência o sobrenome digitado"""
+
+nome = input("Digite seu nome:")
+prof= input("Sua profição:")
+idade = int(input("Informe sua idade:"))
+snome = input("Informe seu Ultimo Sobrenome")
+print("Boa noite,",nome,"!")
+print("Seu nome:",nome,", Profissão:",prof)
+print("Sua idade é:",idade)
+print("Familia",snome)
